745bda51e44d17a9703e0b80c84e471f7e5c5d04
leesanha <bluegisa@daum.net>
2019-03-01 16:43:56 +0900

    a

--- knuwebproject/interview/models.py
+++ knuwebproject/interview/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Interview(models.Model):
     title = models.CharField(max_length = 255)
-    description = models.CharField(max_length=500)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
